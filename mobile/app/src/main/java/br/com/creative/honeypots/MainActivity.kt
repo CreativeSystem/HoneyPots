ce70a25696df5305b721d9e8ad4b581c95cb7ffc
ElielC <eliel-calebi@hotmail.com>
2020-09-03 20:53:11 -0300

    chore: navigation bar created

--- mobile/app/src/main/java/br/com/creative/honeypots/MainActivity.kt
+++ mobile/app/src/main/java/br/com/creative/honeypots/MainActivity.kt
@@ -3,14 +3,40 @@ package br.com.creative.honeypots
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.navigation_menu.*
+import kotlinx.android.synthetic.main.navigation_menu.view.*
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView.bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.feed -> {
+                    Toast.makeText(this, "FEED", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.search -> {
+                    Toast.makeText(this, "SEARCH", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.liked -> {
+                    Toast.makeText(this, "LIKED", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.profile -> {
+                    Toast.makeText(this, "PROFILE", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
 
         btn_dark.setOnClickListener {
             if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO) {
