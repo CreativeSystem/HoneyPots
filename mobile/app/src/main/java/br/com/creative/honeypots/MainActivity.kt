c0c04774dfefef6e80cc7934078febc3b60cd51c
leoferreiralima <leoferreiralima@yahoo.com>
2020-09-05 17:23:26 -0300

    feat: add app splash screen

--- mobile/app/src/main/java/br/com/creative/honeypots/MainActivity.kt
+++ mobile/app/src/main/java/br/com/creative/honeypots/MainActivity.kt
@@ -2,20 +2,22 @@ package br.com.creative.honeypots
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import br.com.creative.honeypots.presentation.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.navigation_menu.view.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragNavHost) as NavHostFragment
---
+++ mobile/app/src/main/java/br/com/creative/honeypots/presentation/BaseActivity.kt
@@ -0,0 +1,11 @@
+package br.com.creative.honeypots.presentation
+
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+open class BaseActivity : AppCompatActivity() {
+
+}
