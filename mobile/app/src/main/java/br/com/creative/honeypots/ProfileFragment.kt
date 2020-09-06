64dc874f0826f51e72d3c444a04440140f6b5376
ElielC <eliel-calebi@hotmail.com>
2020-09-06 11:06:13 -0300

    chore: create fedd top app bar

--- mobile/app/src/main/java/br/com/creative/honeypots/ProfileFragment.kt
+++ mobile/app/src/main/java/br/com/creative/honeypots/ProfileFragment.kt
@@ -4,10 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
-    override fun onCreateView(inflater:LayoutInflater,container: ViewGroup?,savedInstanceState:Bundle?):View?{
-        return inflater.inflate(R.layout.fragment_profile,container,false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_dark.setOnClickListener {
+            if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                btn_dark.text = "Enable Light Mode"
+                return@setOnClickListener;
+            }
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            btn_dark.text = "Enable Dark Mode"
+        }
     }
 }
