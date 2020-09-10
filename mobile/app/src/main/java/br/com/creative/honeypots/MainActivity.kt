e565d9cc649ba19b8c0fb427fcf63e86e2f433eb
Felipe Goulart <felipe.goulart1999@gmail.com>
2020-09-09 21:44:16 -0300

    Item list on feed

--- mobile/app/src/main/java/br/com/creative/honeypots/MainActivity.kt
+++ mobile/app/src/main/java/br/com/creative/honeypots/MainActivity.kt
@@ -25,27 +25,6 @@ class MainActivity : BaseActivity() {
             supportFragmentManager.findFragmentById(R.id.fragNavHost) as NavHostFragment
         val navController = navHostFragment.navController
 
-        val recipes: ArrayList<Recipe> = ArrayList()
-
-        val l = ArrayList<String>()
-        l.add(0,"Ingredient 1")
-        l.add(1,"Ingredient 2")
-        l.add(2,"Ingredient 3")
-        for (i in 1..20) {
-            recipes.add(Recipe("Item #$i", l, R.drawable.image_12, "Description"))
-        }
-
-        with(recipeListView) {
-            layoutManager =
-                LinearLayoutManager(this@MainActivity, RecyclerView.HORIZONTAL, false)
-            setHasFixedSize(true)
-            adapter = RecipeFeedAdapter(recipes) { recipe ->
-                val intent = RecipeDetailActivity
-                    .getStartIntent(this@MainActivity, recipe);
-                this@MainActivity.startActivity(intent);
-            };
-        }
-
         bottomNavigationView.bottomNavigation.setupWithNavController(navController)
 
         bottomNavigationView.floatingButton.setOnClickListener {
